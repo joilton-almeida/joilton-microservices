44729933b8b20b1b11a5455825d77c368c55a70d
Joilton Almeida <joilton@gmail.com>
2022-09-25 19:54:45 -0300

    Todos arquivos do curso de microsserviços com Docker

---
+++ stack-docker/10-cambio-service/src/main/java/br/unb/joilton/controller/CambioController.java
@@ -0,0 +1,56 @@
+package br.unb.joilton.controller;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.unb.joilton.model.Cambio;
+import br.unb.joilton.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Cambio endpoint")
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+	
+	@Autowired
+	Environment environment;
+
+	@Autowired
+	CambioRepository cambioRepository;
+	
+	@Operation(summary = "Get cambio from currency")
+	@GetMapping(value = "/{amount}/{from}/{to}")
+	public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
+			@PathVariable("from") String from,
+			@PathVariable("to") String to) {
+		
+		var cambio = cambioRepository.findByFromAndTo(from, to);
+		
+		if(cambio == null) {
+			
+			throw new RuntimeException("Currency Unsupported");
+			
+		}
+		
+		var port = environment.getProperty("local.server.port");
+		
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		
+		return cambio;
+		
+	}
+
+}
